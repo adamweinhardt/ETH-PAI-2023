3998aaf038d20689c997780334770dab30394cda
Marci0707 <schneider.marcell2@gmail.com>
2023-11-18 21:35:15 +0100

    Task 3: live eval script

---
+++ Task3/eval_run.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+with open('log.txt') as f:
+    content = f.readlines()
+
+scores = []
+for line in content:
+    if line.startswith('Score '):
+        scores.append(float(line.split(':')[1].strip()))
+
+# Calculate the mean of the values
+mean_value = np.mean(scores)
+
+# Create a line plot
+plt.plot(scores, label='Scores')
+
+# Draw a line at the mean
+plt.axhline(y=mean_value, color='r', linestyle='--', label='Mean')
+plt.xticks(np.arange(0, len(scores), 5), np.arange(1, len(scores)+1, 5))
+
+# Add labels and legend
+plt.xlabel('Index')
+plt.ylabel('Values')
+plt.legend()
+
+# Show the plot
+plt.show()
